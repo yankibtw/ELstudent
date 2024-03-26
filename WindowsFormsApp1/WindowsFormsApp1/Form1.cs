2ddfbad2f28bab7b4ddafe623b1dd216499db477
yankibtw <143903957+yankibtw@users.noreply.github.com>
2024-03-26 21:04:08 +0300

    newExplorer

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=Database1.mdf;Initial Catalog=Table;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT * FROM Table", connection);
+                    Console.WriteLine("Соединение установлено успешно!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка при подключении к базе данных: " + ex.Message);
+                }
+            }
+        }
+    }
+}
